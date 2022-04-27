beb79f54f14568a9026220324d0f80e7e824372f
Pierre-Xavier <pierre.xavier@gmail.com>
2022-04-27 10:05:53 +0200

    add code

---
+++ untrash.rb
@@ -0,0 +1,69 @@
+require "google/apis/drive_v3"
+require "googleauth"
+require "googleauth/stores/file_token_store"
+require "fileutils"
+
+OOB_URI = "http://localhost".freeze
+APPLICATION_NAME = "Drive API Ruby Quickstart 2".freeze
+CREDENTIALS_PATH = "credentials.json".freeze
+# The file token.yaml stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+TOKEN_PATH = "token.yaml".freeze
+SCOPE = Google::Apis::DriveV3::AUTH_DRIVE
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or initiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
+  token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
+  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
+  user_id = "default"
+  credentials = authorizer.get_credentials user_id
+  if credentials.nil?
+    url = authorizer.get_authorization_url base_url: OOB_URI
+    puts "Open the following URL in the browser and enter the " \
+         "resulting code after authorization:\n" + url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI
+    )
+  end
+  credentials
+end
+
+# Initialize the API
+drive_service = Google::Apis::DriveV3::DriveService.new
+drive_service.client_options.application_name = APPLICATION_NAME
+drive_service.authorization = authorize
+
+(1..).each do |round|
+  # List 100 files inside the trash
+  puts "list files"
+  response = drive_service.list_files( page_size: 100,
+                                      fields:    "nextPageToken, files(id, name)",
+                                      q: 'trashed=true'
+                                      )
+  puts "Files:"
+  puts "No files found" if response.files.empty?
+  break if response.files.empty?
+  puts "Nb files found #{response.files.count}"
+
+  drive_service.batch do
+    response.files.each_with_index do |file, index|
+      puts "#{file.name} (#{file.id}) (#{index+1}/#{response.files.count} round: #{round})"
+      # puts "before:"
+      # p drive_service.get_file(file.id, fields: "trashed") # just for verification
+
+      file_metadata = {trashed: false}
+      drive_service.update_file(file.id, file_metadata, {})
+      #puts "after:"
+      #p drive_service.get_file(file.id, fields: "trashed") # just for verification
+    end
+    puts "do batch processing..."
+  end
+end
